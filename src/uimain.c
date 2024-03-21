222f271bbb5415e15a850109b41a32bf29073632
Brian Mon <brianmon@Brians-MacBook-Pro-2.local>
2024-03-21 15:11:16 -0600

    added history all and tokenizer

--- src/uimain.c
+++ src/uimain.c
@@ -7,19 +7,27 @@ int main()
    char str [80];
 
    List* list = init_history();
+   char *word = "hello world";
+   char **tokens = tokenize(word);
 
    int quit = 0;
    while(!quit) {
-    printf("Tokenizer! Write anything. To look at your history type !(number), to Quit enter !q\n");
+    printf("Tokenizer! Write anything. To look at all your history type !a for all, or !(number) for a specific history item, to Quit enter !q\n");
     fgets(str, 80, stdin);
     printf("%s\n",str);
 
-    char **tokens = tokenize(str);
-
+    tokens = tokenize(str);
+    printf("%s\n",tokens[0]);
+    print_tokens(tokens);
+    
     if (str[0] == '!') {
         if (str[1] == 'q') {
             return 0;
         }
+
+	if (str[1] == 'a') {
+	  print_history(list);
+	}
 	int num = 0;
         num = str[1] - 48;
         if (get_history(list, num) == NULL) {
